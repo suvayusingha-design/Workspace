24569f5a9fafd49e207de8867a040476c7f2a07d
suvayusingha <suvayusinghahi@gmail.com>
2026-07-27 18:23:33 +0530

    Break in loop

---
+++ C language/break_statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main(){
+  int n;
+  do{
+    printf("enter a number:");
+    scanf("%d",&n);
+    printf("%d\n",n);
+    if (n%2!=0){
+      break;
+    }
+  }while(1);
+  printf("Thank you");
+  return 0;
+ }
