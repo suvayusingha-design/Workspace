b1a2974cec14eb3ba080017a2e8dcca926209e80
suvayusingha <suvayusinghahi@gmail.com>
2026-07-30 00:07:08 +0530

    changes

--- C language/array.c
+++ C language/array.c
@@ -1,11 +1,11 @@
  #include<stdio.h>
  int main(){
     int marks[2];
-    printf("enter marks of A : ");
+    printf("enter marks of a : ");
     scanf("%d",&marks[0]);
-     printf("enter marks of B : ");
+     printf("enter marks of b : ");
      scanf("%d",&marks[1]);
 
-     printf("A=%d,B=%d",marks[0],marks[1]);
+     printf("a=%d,b=%d",marks[0],marks[1]);
      return 0;
  }
