24562da8beab54fefab3d87b764babdb66e82124
suvayusingha <suvayusinghahi@gmail.com>
2026-07-30 00:06:01 +0530

    Array_practice

---
+++ C language/array.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ int main(){
+    int marks[2];
+    printf("enter marks of A : ");
+    scanf("%d",&marks[0]);
+     printf("enter marks of B : ");
+     scanf("%d",&marks[1]);
+
+     printf("A=%d,B=%d",marks[0],marks[1]);
+     return 0;
+ }
---
+++ C language/array_1.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ int main(){
+    float price[3];
+    printf("Enter prices : ");
+    scanf("%f", &price[0]);
+     scanf("%f", &price[1]);
+      scanf("%f", &price[2]);
+      printf("%f\n", price[0]+(0.18*price[0]));
+       printf("%f\n", price[1]+(0.18*price[1]));
+        printf("%f\n", price[2]+(0.18*price[2]));
+        return 0;
+ }
