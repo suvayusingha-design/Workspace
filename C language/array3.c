f52f280311a5394cdd89a524e601026e0871c013
suvayusingha <suvayusinghahi@gmail.com>
2026-07-30 18:15:53 +0530

    Array questions

---
+++ C language/array3.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+ int main(){
+int marks[2][3];
+marks [0][0] = 90;
+marks [0][1] = 99;
+marks [0][2] = 88;
+marks [1][0] = 97;
+marks [1][1] = 96;
+marks [1][2] = 95;
+printf("%d", marks[1][1]);
+return 0;
+ }
+ 
