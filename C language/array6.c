a5aefbc476efb0b1373ba420f572b59c65a0b4e8
suvayusingha <suvayusinghahi@gmail.com>
2026-07-31 23:57:31 +0530

    practice

---
+++ C language/array6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+     
+    int tables[2][10];
+
+     
+    for (int i = 0; i < 10; i++) {
+        tables[0][i] = 2 * (i + 1);  
+        tables[1][i] = 3 * (i + 1);  
+    }
+
+    // Print the stored tables
+    printf("Table of 2: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", tables[0][i]);
+    }
+    
+    printf("\nTable of 3: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", tables[1][i]);
+    }
+
+    return 0;
+}
