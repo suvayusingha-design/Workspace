6bc1e221dbf0e02c0b9c2fe0f13591e0b48aa1a4
suvayusingha <suvayusinghahi@gmail.com>
2026-08-01 23:10:27 +0530

    New add

--- C language/array7.c
+++ C language/array7.c
@@ -19,7 +19,7 @@ int main() {
     return 0;
 }
 
-void storetable(int arr[2][10],int n,int m,int number) {
+void storetable(int arr[][10],int n,int m,int number) {
       for(int i=0;i<m;i++) {
     arr[n][i]=number* (i+1);
 }
