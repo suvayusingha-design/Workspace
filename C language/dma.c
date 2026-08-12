acef982e59960c72c5eb368e8d4c5040adec1c3a
suvayusingha <suvayusinghahi@gmail.com>
2026-08-13 01:57:56 +0530

    add

--- C language/dma.c
+++ C language/dma.c
@@ -6,9 +6,9 @@
     ptr=(int *)malloc(5*sizeof(int));
     ptr[0]=1;
     ptr[1]=2;
-ptr[2]=3;
-ptr[3]=4;
-ptr[4]=5;
+    ptr[2]=3;
+    ptr[3]=4;
+    ptr[4]=5;
     for(int i=0; i<5 ; i++){
         printf("%d\n",ptr[i]);
 
