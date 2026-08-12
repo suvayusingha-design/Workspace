a05738bf448061999d967a2d391a25b95057df69
suvayusingha <suvayusinghahi@gmail.com>
2026-08-12 21:35:18 +0530

    add

---
+++ C language/files3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    fptr = fopen("test.txt","r");
+    char ch;
+    ch = fgetc(fptr);
+while(ch != EOF){
+    printf("%c",ch);
+    ch=fgetc(fptr);
+}
+return 0;
+}
