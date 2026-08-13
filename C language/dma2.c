9989b9d9901eb0f7514548a5af096495cfa0ea52
suvayusingha <suvayusinghahi@gmail.com>
2026-08-14 02:14:59 +0530

    add

---
+++ C language/dma2.c
@@ -0,0 +1,23 @@
+ #include<stdio.h>
+ #include<stdlib.h>
+ int main(){
+    int *ptr;
+    ptr = (int*) calloc (5 ,sizeof(int));
+
+    printf("Enter numbers : ");
+    for(int i=0;i<5;i++){
+        scanf("%d", &ptr[i]);
+    }
+
+    ptr = realloc(ptr, 6 * sizeof(int));
+    printf("Enter numbers : ");
+    for(int i=0;i<6;i++){
+        scanf("%d", &ptr[i]);
+    }
+
+    for(int i=0;i<6;i++){
+        printf("number is %d\n",ptr[i]);
+
+    }
+return 0;
+ }
